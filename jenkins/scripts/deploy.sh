e72f829ffe0cd293eaf408b3680360dc86f052e0
Wangu <90233441+tohxuewang99@users.noreply.github.com>
2023-11-14 22:28:33 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,11 +1,30 @@
+# #!/usr/bin/env sh
+
+# set -x
+# docker run -d -p 80:80 --name my-apache-php-app -v selenium_test\src:/var/www/html php:7.2-apache
+# sleep 1
+# set +x
+
+# echo 'Now...'
+# echo 'Visit http://localhost to see your PHP application in action.'
+
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v selenium_test\src:/var/www/html php:7.2-apache
+
+# Set correct permissions
 chmod -R 755 selenium_test/src
+
+# Set correct ownership
+chown -R www-data:www-data selenium_test/src
+
+# Run Docker container
+docker run -d -p 80:80 --name my-apache-php-app -v "$(pwd)/selenium_test/src:/var/www/html" php:7.2-apache
+
+# Allow some time for the container to start
 sleep 1
+
 set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-
