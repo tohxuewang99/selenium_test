86354d632b683006e76d357857ea8ed5c541d28c
Wangu <90233441+tohxuewang99@users.noreply.github.com>
2023-11-14 22:36:42 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -12,11 +12,6 @@
 
 set -x
 
-# Set correct permissions
-chmod -R 755 ../../src
-
-# Set correct ownership
-chown -R www-data:www-data ../../src
 
 # Run Docker container
 docker run -d -p 80:80 --name my-apache-php-app -v "$(pwd)/selenium_test/src:/var/www/html" php:7.2-apache
